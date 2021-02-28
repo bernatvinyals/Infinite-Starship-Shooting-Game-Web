2263f925d24532e05e076e40a860555b59b9dc5e
sangot <36930141+sangotxd@users.noreply.github.com>
2021-02-28 21:41:10 +0100

    UI Added, Sound Added

--- game.js
+++ game.js
@@ -17,19 +17,44 @@ function Ship(X,Y,W,H){
 	var img;
 	var Life = 3;
 }
-
+function Misile(X,Y,W,H){
+	this.PosX = X;
+	this.PosY = Y;
+	this.Width = W;
+	this.Height = H;
+	var img;
+}
 var Player = new Ship (320,400,80,59);
+Player.Life = 3;
+var Points=0;
 var Enemy = [
-	new Ship ((Math.random()*600),(Math.random()*-400),50,90),
-	new Ship ((Math.random()*600),(Math.random()*-400),50,90),
-	new Ship ((Math.random()*600),(Math.random()*-400),50,90),
-	new Ship ((Math.random()*600),(Math.random()*-400),50,90),
-	new Ship ((Math.random()*600),(Math.random()*-400),50,90)
+	new Ship ((Math.random()*600),(Math.random()*-400),60,59),
+	new Ship ((Math.random()*600),(Math.random()*-400),60,59),
+	new Ship ((Math.random()*600),(Math.random()*-400),60,59),
+	new Ship ((Math.random()*600),(Math.random()*-400),60,59),
+	new Ship ((Math.random()*600),(Math.random()*-400),60,59)
 ];
+
+var Misiles = [
+	new Misile (0,0,60,59),
+	new Misile (0,0,60,59),
+	new Misile (0,0,60,59),
+	new Misile (0,0,60,59),
+	new Misile (0,0,60,59)
+];
+
+var HitSound;
+var HitSoundR;
+var ShootSound;
+var ShootSoundR;
+var BGMusic;
+
 
 
 var scene = 0;	
-var img = {}
+var img = {};
+
+var restartGame= false;
 function joc(){
 	switch(scene){
 		case 0:
@@ -41,6 +66,9 @@ function joc(){
 		case 2:
 			loopGame();
 			break;
+		case 4:
+		//cagaste!
+			break;
 	}
 
 	requestAnimationFrame(joc);
@@ -48,6 +76,37 @@ function joc(){
 
 
 function inicialitzacioICarregaImatges(){
+	HitSound = document.createElement("audio");
+	HitSound.src = "./sounds/HitSound.mp3";
+	HitSound.setAttribute("preload", "auto");
+	HitSound.setAttribute("controls", "none");
+	HitSound.style.display = "none";
+
+	HitSoundR = document.createElement("audio");
+	HitSoundR.src = "./sounds/HitSound.mp3";
+	HitSoundR.setAttribute("preload", "auto");
+	HitSoundR.setAttribute("controls", "none");
+	HitSoundR.style.display = "none";
+
+	ShootSound = document.createElement("audio");
+	ShootSound.src = "./sounds/HitSound.mp3";
+	ShootSound.setAttribute("preload", "auto");
+	ShootSound.setAttribute("controls", "none");
+	ShootSound.style.display = "none";
+
+	ShootSoundR = document.createElement("audio");
+	ShootSoundR.src = "./sounds/HitSound.mp3";
+	ShootSoundR.setAttribute("preload", "auto");
+	ShootSoundR.setAttribute("controls", "none");
+	ShootSoundR.style.display = "none";
+
+	BGMusic = document.createElement("audio");
+	BGMusic.src = "./sounds/bgMusic.mp3";
+	BGMusic.setAttribute("preload", "auto");
+	BGMusic.setAttribute("controls", "none");
+	BGMusic.style.display = "none";
+
+
 	img[0] = document.createElement("img");
 	img[0].src = "./image/bg/bg1.png";
 	img[1] = document.createElement("img");
@@ -61,6 +120,10 @@ function inicialitzacioICarregaImatges(){
 	for (var i =0; i <  Enemy.length; i++) {
 		Enemy[i].img = document.createElement("img");
 		Enemy[i].img.src = "./image/enemy.png";
+	}	
+	for (var i =0; i <  Misiles.length; i++) {
+		Misiles[i].img = document.createElement("img");
+		Misiles[i].img.src = "./image/missile.png";
 	}
 
 	scene = 1;
@@ -68,14 +131,75 @@ function inicialitzacioICarregaImatges(){
 
 function esperarCarregarImatges(){
 	var loading = false;
-	if (Player.img.complete == true && img[1].complete&& img[3].complete && img[0].complete && img[2].complete && Enemy[0].img.complete == true && Enemy[1].img.complete == true && Enemy[2].img.complete == true && Enemy[3].img.complete == true && Enemy[4].img.complete == true){
+	if (Player.img.complete == true && img[1].complete&& img[3].complete && img[0].complete && img[2].complete && Enemy[0].img.complete == true && Enemy[1].img.complete == true && Enemy[2].img.complete == true && Enemy[3].img.complete == true && Enemy[4].img.complete == true&& Misiles[0].img.complete == true && Misiles[1].img.complete == true && Misiles[2].img.complete == true && Misiles[3].img.complete == true && Misiles[4].img.complete == true){
 		loading = true;
 	}
 	
 	if (loading == true){
+		BGMusic.play();
 		scene = 2;
 	}
 }
+
+
+
+var ticksForSpacebar =101;
+var bulletIndex=0;
+var audioAlradyPlayingShoot=false;
+function shoot(){
+	if (ticksForSpacebar >= 50) {	
+		ticksForSpacebar=0;
+		if (audioAlradyPlaying) {
+			ShootSoundR.play();
+			audioAlradyPlayingShoot = false;
+		}
+		else{
+			audioAlradyPlayingShoot = true,
+			ShootSound.play();
+		}
+		Misiles[bulletIndex].PosX = Player.PosX + 25;
+		Misiles[bulletIndex].PosY = Player.PosY;
+		bulletIndex += 1;
+		if (bulletIndex == (Misiles.length)-1) {
+			bulletIndex = 0;
+		}
+	}
+}
+var audioAlradyPlaying = false;
+function checkForShooting(){
+	for (var i = 0; i < Misiles.length; i++) {
+		if (Misiles[i].PosY >= 0) {
+			Misiles[i].PosY= Misiles[i].PosY-3;
+			for (var iy = 0; iy < Enemy.length; iy++) {
+				if (CheckCollision(Misiles[i],Enemy[iy])) {
+					Misiles[i].PosX = -90;
+					Misiles[i].PosY = -90;
+					Enemy[iy].PosX = (Math.random()*600);
+					Enemy[iy].PosY = (Math.random()*-400);
+					Points = Points+ 100;
+					if (audioAlradyPlaying) {
+						HitSoundR.play();
+						audioAlradyPlaying = false;
+					}
+					else{
+						audioAlradyPlaying = true,
+						HitSound.play();
+					}
+				}
+			}
+		}else{
+			Misiles[i].PosX = -90;
+			Misiles[i].PosY = -90;
+		}
+	}
+}
+function showMisiles(ctx){
+	for (var i = 0; i < Misiles.length; i++) {
+		ctx.drawImage(Misiles[i].img,Misiles[i].PosX,Misiles[i].PosY);
+	}
+	ticksForSpacebar = ticksForSpacebar +1;
+}
+
 
 
 var bgY=240;
@@ -89,12 +213,14 @@ var dirVector=320;
 var spacebar;
 var rightArrow;
 var leftArrow;
+
 function movPlayer(){
     $(document).ready(function(){
         $(document).keydown(function(event){
             var keycode = (event.keycode ? event.keyCode : event.which);
             if(keycode==32){
-                spacebar=true;
+            	spacebar=true;
+
             }
             if(keycode==39){
                 rightArrow=true;
@@ -119,7 +245,9 @@ function movPlayer(){
         });
         
     });
-    
+    if (spacebar) {
+    	shoot();
+    }
     if(rightArrow==true){
         dirVector=dirVector+5;    
         Player.PosX=dirVector;     
@@ -164,7 +292,11 @@ function enemyMov(){
 	}
 }
 
-function respEnemy(){
+function respEnemy(ctx){
+	for (var i = 0; i < Enemy.length; i++) {
+		ctx.drawImage(Enemy[i].img,Enemy[i].PosX,Enemy[i].PosY);
+	}
+
 	for (var i = 0; i < Enemy.length; i++) {
 		if (CheckCollision(Player,Enemy[i])) {
 			Enemy[i].PosX = (Math.random()*600);
@@ -175,10 +307,14 @@ function respEnemy(){
 			}
 		}
 	}
-	
 }
 
+
 function loopGame(){
+	if (restartGame) {
+		Player.Life = 3;
+		Points = 0;
+	}
 	var jocActiu = true; 
 	var canvas = document.getElementById("myCanvas");
 	var ctx = canvas.getContext("2d");
@@ -186,22 +322,26 @@ function loopGame(){
 	repeatBG();
 	movPlayer();
 	enemyMov();
-	
-	ctx.clearRect(0,0,640,480);	
-	ctx.drawImage(img[0],0,0);
-	ctx.drawImage(img[2],bg2X,(bg2Y=bg2Y + 1));
-	ctx.drawImage(img[3],bg3X,(bg3Y=bg3Y + 1));
-	ctx.drawImage(img[1],bgX,(bgY=bgY + 2));
+	checkForShooting();
 
+	ctx.clearRect(0,0,640,480);	
+	//Creating parallax
+	ctx.drawImage(img[0],0,0);
+	ctx.drawImage(img[2],bg2X,(bg2Y=bg2Y + 0.5));
+	ctx.drawImage(img[3],bg3X,(bg3Y=bg3Y + 1));
+	ctx.drawImage(img[1],bgX,(bgY=bgY + 1.5));
+	//Out of bounds check
 	if (Player.PosX <=0) {
 		Player.PosX = 0;
 	}if (Player.PosX >=640) {
 		Player.PosX = 640;
 	}
-
-	console.log(Enemy[0].PosX + " " +Enemy[0].PosY);
-	ctx.drawImage(Enemy[0].img,Enemy[0].PosX,Enemy[0].PosY)
 	
-	ctx.drawImage(Player.img, Player.PosX, Player.PosY);
-	
+	respEnemy(ctx); //Showing Enemies
+	showMisiles(ctx);
+	ctx.drawImage(Player.img, Player.PosX, Player.PosY); //Showing Player
+	ctx.font = "47px DotGothic16";
+	ctx.fillStyle = "white";
+	ctx.fillText(("Lives: "+ Player.Life), 10, 50); 
+	ctx.fillText(("Points: "+ Points), 10, 100); 
 }
